eb20e90f62573586e2323c8a75ef4e9ef6d20892
persocom01 <persocom02@gmail.com>
2020-02-20 16:20:41 +0800

    0.17

--- 05_where_and_or_test.py
+++ 05_where_and_or_test.py
@@ -5,22 +5,26 @@ from sqlalchemy import create_engine
 engine = create_engine('mysql+pymysql://root:@localhost/testDB')
 con = engine.connect()
 
-# Comparison operators in sql:
-# =
-# !=
-# <> same as !=
-# >
-# !> not greater than
-# <
-# !< not smaller than
-# >=
-# <=
-
 command = '''
-SELECT name,race,age
+SELECT name,class,race,age
 FROM konosuba
 WHERE age > 18;
 '''
 df = pd.read_sql_query(command, engine)
+print('age > 18')
+print(df.head())
+print()
+
+# Note that BOOLEAN columns don't need a value = 1 or value = True, but it does
+# still work if written that way.
+# Brackets are optional in this case, but makes the logic clearer, as AND has
+# higher priority over OR.
+command = '''
+SELECT name,class,race,age
+FROM konosuba
+WHERE (NOT male AND race = 'human') OR class = 'adventurer';
+'''
+df = pd.read_sql_query(command, engine)
+print('not male AND human OR class = adventurer')
 print(df.head())
 print()
