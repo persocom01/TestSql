7213fe76446524882cb2f68cacd9f1f4075c0aa8
persocom01 <persocom02@gmail.com>
2020-02-21 05:14:50 +0800

    0.18

--- 05_where_and_or_test.py
+++ 05_where_and_or_test.py
@@ -22,19 +22,9 @@ print()
 command = '''
 SELECT name,class,race,age
 FROM konosuba
-WHERE (NOT male AND race = 'human') OR class = 'adventurer';
+WHERE (NOT isekai AND race = 'human') OR class = 'adventurer';
 '''
 df = pd.read_sql_query(command, engine)
-print('not male AND human OR class = adventurer')
-print(df.head())
-print()
-
-command = '''
-SELECT name,class,race,age
-FROM konosuba
-WHERE (NOT male AND race = 'human') OR class = 'adventurer';
-'''
-df = pd.read_sql_query(command, engine)
-print('not male AND human OR class = adventurer')
+print('not isekai AND human OR class = adventurer')
 print(df.head())
 print()
