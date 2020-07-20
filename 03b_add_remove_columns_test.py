35ceef31473f04ae4e5582683f2ca0e4c40d0e40
Toh <clarenceboonteck.toh@dxc.com>
2020-07-20 11:51:31 +0800

    0.45

--- 03b_add_remove_columns_test.py
+++ 03b_add_remove_columns_test.py
@@ -64,11 +64,38 @@ print(cz.insert_columns('konosuba_copy', 'client', cols='name', where='M', print
 print(cursor.execute(command))
 
 command = '''
-CREATE PROCEDURE MIMIC.test(CREATE PROCEDURE MIMIC.map_icd9_to_icd10_am())
+CREATE PROCEDURE MIMIC.test(CREATE PROCEDURE MIMIC.map_icd9_to_icd10_am(IN target VARCHAR(255)))
 BEGIN
     SET @table_name = target;
     SET @command = concat('SELECT * FROM ',@table_name);
 
+    PREPARE stmt FROM @command;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END
+'''
+print(cz.insert_columns('konosuba_copy', 'client', cols='name', where='M', printable=True))
+print(cursor.execute(command))
+
+command = '''
+CREATE PROCEDURE MIMIC.map_icd9_pcs_to_icd10_pcs(IN target VARCHAR(255))
+BEGIN
+    SET @table_name = target;
+    SET @command = concat('ALTER TABLE ',@table_name,' ADD ICD10_CODE VARCHAR(10800);');
+    PREPARE stmt FROM @command;
+
+    IF NOT EXISTS (
+        SELECT NULL
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name = @table_name
+        AND table_schema = 'MIMIC'
+        AND column_name = 'ICD10_CODE'
+        ) THEN
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+
+    SET @command = concat('UPDATE ',@table_name,' t JOIN MIMIC.D_ICD_PROCEDURES_copy s ON s.ICD9_CODE = t.ICD9_CODE SET t.ICD10_CODE = s.ICD10_CODE;');
     PREPARE stmt FROM @command;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
